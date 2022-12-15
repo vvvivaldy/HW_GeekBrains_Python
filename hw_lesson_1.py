eaf1fe373f433d6a806e0a4d12a1b09bcb6046ae
NikitaGutenev <gutenev229@gmail.com>
2022-12-15 19:24:24 +0300

    сделал №1 и №2

---
+++ hw_lesson_1.py
@@ -0,0 +1,14 @@
+#1
+
+# day = int(input("Введите день недели: "))
+# print(day==6 or day==7)
+
+
+#2
+# print ("x y z -> result")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             print(f"{x} {y} {z} -> ", not(x+y+z>0)==(not(x) and not(y) and not(z)))
+
+#3
