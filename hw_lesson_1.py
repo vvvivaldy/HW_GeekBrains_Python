18d45f8f500614f5e821136d14dded6f3a02d50c
NikitaGutenev <gutenev229@gmail.com>
2022-12-16 13:20:53 +0300

    №3

--- hw_lesson_1.py
+++ hw_lesson_1.py
@@ -12,3 +12,20 @@
 #             print(f"{x} {y} {z} -> ", not(x+y+z>0)==(not(x) and not(y) and not(z)))
 
 #3
+x = int(input("Enter X: "))
+y = int(input("Enter Y: "))
+
+if x>0 and y>0:
+    print(1)
+elif x<0 and y>0:
+    print(2)
+elif x<0 and y<0:
+    print(3)
+elif x>0 and y<0:
+    print(4)
+elif x==0 and y!=0:
+    print("На оси X")
+elif y==0 and x!=0:
+    print("На оси Y")
+else:
+    print("В начале координат")
