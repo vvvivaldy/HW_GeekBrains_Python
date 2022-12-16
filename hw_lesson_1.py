32f48d208b060df54e8ec05e826b0dd8efb3b808
NikitaGutenev <gutenev229@gmail.com>
2022-12-16 13:42:42 +0300

    №5

--- hw_lesson_1.py
+++ hw_lesson_1.py
@@ -12,20 +12,44 @@
 #             print(f"{x} {y} {z} -> ", not(x+y+z>0)==(not(x) and not(y) and not(z)))
 
 #3
-x = int(input("Enter X: "))
-y = int(input("Enter Y: "))
+# x = int(input("Enter X: "))
+# y = int(input("Enter Y: "))
 
-if x>0 and y>0:
-    print(1)
-elif x<0 and y>0:
-    print(2)
-elif x<0 and y<0:
-    print(3)
-elif x>0 and y<0:
-    print(4)
-elif x==0 and y!=0:
-    print("На оси X")
-elif y==0 and x!=0:
-    print("На оси Y")
-else:
-    print("В начале координат")
+# if x>0 and y>0:
+#     print(1)
+# elif x<0 and y>0:
+#     print(2)
+# elif x<0 and y<0:
+#     print(3)
+# elif x>0 and y<0:
+#     print(4)
+# elif x==0 and y!=0:
+#     print("На оси X")
+# elif y==0 and x!=0:
+#     print("На оси Y")
+# else:
+#     print("В начале координат")
+
+#4
+# y = int(input("Enter: "))
+# if y==1:
+#     print('x(0;+бесконечость) y(0;+бесконечость)')
+# elif y==2:
+#     print('x(0;-бесконечость) y(0;+бесконечость)')
+# elif y==3:
+#     print('x(0;-бесконечость) y(0;-бесконечость)')
+# elif y==4:
+#     print('x(0;+бесконечость) y(0;-бесконечость)')
+
+#5
+import math
+xa = int(input('Enter Xa: '))
+ya = int(input('Enter Ya: '))
+xb = int(input('Enter Xb: '))
+yb = int(input('Enter Yb: '))
+
+print(f'A({xa};{ya}), B({xb};{yb}) -> {round(math.sqrt((xb-xa)**2+(yb-ya)**2),2)}')
+
+
+
+
