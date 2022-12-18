dd73b1af11b074770b9c308eec81c777a12ceb66
NikitaGutenev <gutenev229@gmail.com>
2022-12-18 15:43:14 +0300

    сделал пару задач для второго семинара

---
+++ hw_lesson_2.py
@@ -0,0 +1,46 @@
+# #1
+
+# n=float(input())
+# num=str(n)
+# summ = 0
+# for i in num:
+#     if i.isdigit():
+#         summ+=int(i)
+# print(summ)
+
+
+#2
+
+# n = int(input())
+# summ=1
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         summ*=j
+#     print(summ,end=' ')
+#     summ=1
+
+
+
+#3
+
+# n = int(input())
+# summ=0
+# for i in range(1,n+1):
+#     summ+=(1+1/i)**i
+# print(summ)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
