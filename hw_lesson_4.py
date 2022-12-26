eb7602fb864adf748f8a4e61879f90c43889a1af
NikitaGutenev <gutenev229@gmail.com>
2022-12-26 18:45:42 +0300

    сделал 4ю задачу

--- hw_lesson_4.py
+++ hw_lesson_4.py
@@ -24,3 +24,56 @@
 #             n2.discard(n[i])
 # print('Вот список без повторяющихся элементов: ',list(n2))
     
+#4
+import random
+def create(poly,poly2): #Создадим не 1, а два файла с многочленами для решения следующей задачи
+    with open('polynomial_1.txt(hw_4)','w') as data:
+        data.write(poly)
+    with open('polynomial_2.txt(hw_4)','w') as data:
+        data.write(poly2)
+
+def rn(n): #создаем рандомные коэффиценты
+    koef = [random.randint(0,100) for i in range(n+1)]
+    return koef
+
+def outpoly(k,koef): # создаем многочлены
+    res = []
+    for i in range(k,-1,-1):
+
+        if (i<k) and (koef[i]!=0):
+            res.append(' + ')
+
+        if koef[i]==0:
+            res.append('')
+
+        elif (koef[i]==1) and (i!=1) and (i!=0):
+            res.append(f'x**{i}')
+
+        elif (koef[i]!=0) and (i!=1) and (i!=0):
+            res.append(f'{str(koef[i])}x**{str(i)}')
+        
+        elif (koef[i]!=0) and (i==1):
+            res.append('x')
+        
+        elif (koef[i]!=0) and (i==0):
+            res.append(str(koef[i]))
+
+        elif (koef[i]==1) and i==1 or i==0:
+            res.append('1')
+
+    res.append(' = 0') 
+
+    result = ''.join(res) # превращяем список в строку
+
+    if result[0]==' ':
+        result = result.replace(' + ','',1) #удаляем лишний плюс если он есть
+
+    return result
+    
+
+
+
+k = int(input("Введите степень: "))
+
+create(outpoly(k,rn(k)),outpoly(k,rn(k)))
+
