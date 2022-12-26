6c6d89448cdc1f7193c14fd7edcef508ea310d0f
NikitaGutenev <gutenev229@gmail.com>
2022-12-26 16:30:45 +0300

    complete 3 task

--- hw_lesson_4.py
+++ hw_lesson_4.py
@@ -1,4 +1,26 @@
 #1 
-import math
-d = input()
-print(math.round(math.pi(),len(d)-1))
+# import math
+# d = input()
+# print(round(math.pi,len(d)-2))
+
+#2
+# n = int(input())
+# list1 = []
+# while n!=0:
+#     for i in range(2,n+1):
+#         if n%i==0:
+#             list1.append(i)
+#             break
+#     n//=list1[-1]
+# print(list1)
+
+
+#3
+# n = list(map(int,input('Введите элементы списка: ').split()))
+# n2 = set(n)
+# for i in range(len(n)):
+#     for j in range(i+1,len(n)):
+#         if n[i]==n[j]:
+#             n2.discard(n[i])
+# print('Вот список без повторяющихся элементов: ',list(n2))
+    
