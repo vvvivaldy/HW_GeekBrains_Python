16b140da14fb7bbd668a0bb2e3ffc1dc503b6f72
NikitaGutenev <gutenev229@gmail.com>
2023-01-23 15:14:55 +0300

    сделал все здаачи после 6-го семинара

--- hw_lesson_6.py
+++ hw_lesson_6.py
@@ -13,6 +13,38 @@
 # print(vector)
 # print(koren(stepen(vector[0][1] - vector[0][0]) + stepen(vector[1][1] - vector[1][0])))
 
+# #2
+# list1 = ['1','2','3','4','5','6','7','8','9','10']
+# list1 = list(map(int,list1))
+# list2 = list(filter(lambda x: list1.index(x)%2,list1))
+# print(sum(list2))
+
+#3
+# list1 = [i for i in range(1,11)]
+# slovar = dict(enumerate(list1))
+# mult = lambda a,b: a*b
+# sum = 0
+# key = list(slovar.keys())
+
+# for i in range(len(list1)//2):
+#     sum += mult(list1[min(key)],list1[max(key)])
+#     del key[0]
+#     del key[-1]
+
+# if len(list1)%2:
+#     sum+=list1[len(list1)//2]
+
+# print(sum)
+
+
+
+#4
+# n = int(input())
+# s =1
+# f = lambda x: x*(-3)
+# for i in range(0,n):
+#     print(s,end = ' ')
+#     s = f(s)
 
 
 
