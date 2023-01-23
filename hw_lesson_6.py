c5ce46f2df8fa7c822e465703f60df1633a01255
NikitaGutenev <gutenev229@gmail.com>
2023-01-23 13:58:01 +0300

    сделал 1-ю задачу 6

---
+++ hw_lesson_6.py
@@ -0,0 +1,18 @@
+# #1
+# xa = int(input("Введите координату X1: "))
+# ya = int(input("Введите координату Y1: "))
+# xb = int(input("Введите координату X2: "))
+# yb = int(input("Введите координату Y2: "))
+
+# A = (xa,ya)
+# B = (xb,yb)
+
+# vector = list(zip(A,B))
+# stepen = lambda a: a**2
+# koren = lambda a: a**0.5
+# print(vector)
+# print(koren(stepen(vector[0][1] - vector[0][0]) + stepen(vector[1][1] - vector[1][0])))
+
+
+
+
