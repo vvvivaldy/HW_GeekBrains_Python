6acc31c0cab0381a06923ee8a185e2e95b8cd237
NikitaGutenev <gutenev229@gmail.com>
2023-01-29 10:30:26 +0300

    работа готова, осталось добавить фукнцию игры с искуственным интелектом

--- hw_lesson_9/main.py
+++ hw_lesson_9/main.py
@@ -7,96 +7,105 @@ bot = telebot.TeleBot('5976861589:AAEjN6b1tDlQLvJwVX7RiaiaP1UnDCA2z1Y')
 step = randint(1,2) #далее: четное - игрок,нечетное - бот
 candy = 221
 move = randint(1,28)
-a = 0
-next = "Продолжить"
+starter = 0
+winner = None
 
-
-@bot.message_handler(commands=['start','help']) #объявляет реакции на команды
+@bot.message_handler(commands=['start','help','Помощь']) #объявляет реакции на команды
 def start(message):
     bot.send_message(message.chat.id,''' 
     Взгляни на список комманд:
     /start - запуск бота
     /help - список команд
-    /game - запуск игры
-    /restart - перезапускает игру
-    /end - заканчивает игру''')
+    /game - запуск игры''')
 
-@bot.message_handler(commands=['game'])
+@bot.message_handler(commands=['game','Продолжить'])
 def game(message):
-    global a #работает исправно
+    global starter #работает исправно
     global step #рабоатет исправно, меняет вхождения как надо
     global candy #работает исправно
     global move #работает исправно
-    global next # нихуя не рабоает,че за пиздец
+    global winner #рабоатет исправно
 
-    if a == 0:
+    if starter == 0:
         bot.send_message(message.chat.id,'Ну давай поиграем ;)') # отправляет сообщение
-    while candy>0:
-        step+=1
-        if next == 'Продолжить':
-            if not step%2:
-                if a == 0:
-                    bot.send_message(message.chat.id,'Всего конфет 221\n\nТы ходишь первый!')
-                    a = 1
-                    print(8)
-                bot.send_message(message.chat.id,f'Сколько конфет ты возьмёшь?Но не более 28!')
-                bot.register_next_step_handler(message,go) ### регистрирует то,что отправил пользователь.Первый аргумент - то,что передается как агумент в функцию, Второй аргумент - вызываемая ф-ция
-                bot.register_next_step_handler(message,next_move)
-                bot.send_message(message.chat.id,f'Осталось конфет {candy}\nТеперь мой ход!')
-                print(next,1)
-            else:
-                if a == 0:
-                    bot.send_message(message.chat.id,'Всего конфет 221\n\nЯ хожу первый!')
-                    a = 1
-                move = randint(1,28)
-                candy-=move
-                bot.send_message(message.chat.id,f'Я взял {move}\nОсталось конфет {candy}\nТеперь твой ход!')
-                print(next,0)
-        elif next == 'Закончить':
+    if not step%2:
+        if starter == 0:
+            bot.send_message(message.chat.id,'Всего конфет 221\n\nТы ходишь первый!')
+            starter = 1
+        bot.send_message(message.chat.id,f'Сколько конфет ты возьмёшь?Но не более 28!')
+        bot.register_next_step_handler(message,go) ### регистрирует то,что отправил пользователь.Первый аргумент - то,что передается как агумент в функцию, Второй аргумент - вызываемая ф-ция
+        if winner == 1:
             return
+    else:
+        if starter == 0:
+            bot.send_message(message.chat.id,'Всего конфет 221\n\nЯ хожу первый!')
+            starter = 1
+        if candy>28:
+            move = randint(1,28)
         else:
-            continue
+            move = randint(1,candy)
+        candy-=move
+        if candy == 0:
+            bot.send_message(message.chat.id,f'Я взял {move}\nОсталось конфет {candy}')
+            bot.send_message(message.chat.id,f'Игра окончена! Победитель: Бот')
+            return
+        bot.send_message(message.chat.id,f'Я взял {move}\nОсталось конфет {candy}\nТеперь твой ход!')
+        step+=1
+        button(message)
 
-def next_move(message):
-    global next
-    next = message.text
 
-def go(message): #в душе не ебу работает,или нет. Вроде бы да
-    global step,candy,next
+
+def go(message): #работает
+    global step,candy,winner
     move_person = int(message.text)
     candy -= move_person
-    #bot.send_message(message.chat.id,f'Осталось конфет {candy}\nТеперь мой ход!')
-    print(step)
+    if candy == 0:
+            bot.send_message(message.chat.id,f'Игра окончена! Победитель: Ты')
+            winner = 1
+            return
+    bot.send_message(message.chat.id,f'Осталось конфет {candy}\nТеперь мой ход!')
+    step+=1 
     if not step%2!=True:
         button(message)
-        print(123)
 
 
 def button(message): #работает как надо
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    but1 = types.KeyboardButton('Продолжить')
-    but2 = types.KeyboardButton('Закончить')
+    but1 = types.KeyboardButton('/Продолжить')
+    but2 = types.KeyboardButton('/Закончить')
+    but3 = types.KeyboardButton('/Перезапустить')
+    but4 = types.KeyboardButton('/Помощь')
     markup.add(but1)
     markup.add(but2)
+    markup.add(but3)
+    markup.add(but4)
     bot.send_message(message.chat.id,'Нажми кнопку', reply_markup=markup)
 
 
-@bot.message_handler(commands=['restart']) #рабоатет, но надо будет перепроверить после фикса основной части
+@bot.message_handler(commands=['Перезапустить']) #рабоатет, но надо будет перепроверить после фикса основной части
 def restart(message):
     bot.send_message(message.chat.id,'Перезапуск...')
     global step
     global candy
     global move
-    global a
+    global starter
     step = randint(1,2) 
     candy = 221
     move = randint(1,28)
-    a = 0
+    starter = 0
     game(message)
 
-# @bot.message_handler(commands=['end'])
-# def end(message):
-#     bot.send_message(message.chat.id,'Игра окончена!')
+@bot.message_handler(commands=['Закончить'])
+def end(message):
+    global step
+    global candy
+    global move
+    global starter
+    step = randint(1,2) 
+    candy = 221
+    move = randint(1,28)
+    starter = 0
+    bot.send_message(message.chat.id,'Игра окончена!')
 
 
 
