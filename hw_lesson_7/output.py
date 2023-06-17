622f85f59d4c43688218ee6915221fa3c29a74dd
Harpy <gutenev229@gmail.com>
2023-06-17 14:09:27 +0300

    klasjdfs;f

--- hw_lesson_7/output.py
+++ hw_lesson_7/output.py
@@ -1,5 +1,5 @@
 def out_file():
-    data = open('hw_lesson_7/справочник.txt','r')
+    data = open('d:/1_GoGoCode/GoGoPython/HW_Python_GeekBrains/hw_lesson_7/справочник.txt','r')
     for item in data:
         print(item, end='')
     print('\n')
