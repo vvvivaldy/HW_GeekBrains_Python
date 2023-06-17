d38b400a42dec0bff3e0790cea120cc3f6c6da5a
Harpy <gutenev229@gmail.com>
2023-06-17 14:19:23 +0300

    edit 1

--- hw_lesson_1.py
+++ hw_lesson_1.py
@@ -1,15 +1,3 @@
-#1
-
-# day = int(input("Введите день недели: "))
-# print(day==6 or day==7)
-
-
-#2
-# print ("x y z -> result")
-# for x in range(2):
-#     for y in range(2):
-#         for z in range(2):
-#             print(f"{x} {y} {z} -> ", not(x+y+z>0)==(not(x) and not(y) and not(z)))
 
 #3
 # x = int(input("Enter X: "))
